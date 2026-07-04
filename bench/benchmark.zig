7f5ae148f336ce28f7cc63e444aec2b4a75c642c
Ved Jain <vedjain773@gmail.com>
2026-07-04 23:02:19 +0530

    added ifft to the benchmark

--- bench/benchmark.zig
+++ bench/benchmark.zig
@@ -5,6 +5,7 @@ const Complex = std.math.complex.Complex;
 const zig_fft = @import("zig_fft");
 const dft = zig_fft.dft;
 const fft = zig_fft.fft;
+const ifft = zig_fft.ifft;
 
 pub fn benchmark(io: std.Io, comptime size: usize) void {
     const curr_seed: u64 = 1;
@@ -27,11 +28,17 @@ pub fn benchmark(io: std.Io, comptime size: usize) void {
     end = benchtime(io).toMicroseconds();
 
     const fft_time_in_s = (end - start);
-
-    const gain = @divTrunc(dft_time_in_s, fft_time_in_s);
+   
+    var output_ifft: [size] Complex(f32) = undefined;
     
-    std.debug.print("Size: {}    DFT: {}    FFT: {}    Gain: {}\n",
-        .{size, dft_time_in_s, fft_time_in_s, gain});
+    start = benchtime(io).toMicroseconds();
+    ifft(&input, &output_ifft);
+    end = benchtime(io).toMicroseconds();
+
+    const ifft_time_in_s = (end - start);
+
+    std.debug.print("Size: {}    DFT: {}    FFT: {}    IFFT: {}\n",
+        .{size, dft_time_in_s, fft_time_in_s, ifft_time_in_s});
 }
 
 fn benchtime(io: std.Io) std.Io.Timestamp {
